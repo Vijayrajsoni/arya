e1a5ebed4756a5d6a06424afa2c9d54a628f6f6c
vijay raj soni <167947189+Vijayrajsoni@users.noreply.github.com>
2026-09-12 11:22:24 +0530

    Update MongoDB connection error handling
    Change error handling to throw error instead of exiting.

--- dbconnect.js
+++ dbconnect.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 
 const getconnect = async () => {
+
     try {
+
         await mongoose.connect(process.env.MONGO_URI);
 
         console.log("MongoDB connected successfully");
 
         return mongoose.connection.db;
+
     } catch (error) {
+
         console.error("MongoDB connection error:", error);
-        process.exit(1);
+
+        throw error;
     }
 };
 
